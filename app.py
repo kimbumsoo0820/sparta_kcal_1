2b86601356bf0b288db048d6f602986f01c60874
Your Name <you@example.com>
2021-09-14 20:48:15 +0900

    login success

--- app.py
+++ app.py
@@ -15,8 +15,9 @@ app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
 SECRET_KEY = 'SPARTA'
 
 client = MongoClient('localhost', 27017)
-db = client.dbsparta_plus_week4
-
+db = client.todayKcal
+# client = MongoClient('13.209.47.121', 27017, username="test", password="test")
+# db = client.dbsparta_1stminiproject
 
 
 @app.route('/')
@@ -24,13 +25,14 @@ def home():
     token_receive = request.cookies.get('mytoken')
     try:
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-
-        return render_template('index.html')
+        user_info = db.users.find_one({"username": payload["id"]})
+        return render_template('index.html', user_info=user_info)
     except jwt.ExpiredSignatureError:
         return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
     except jwt.exceptions.DecodeError:
         return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
 
+# 로그인 페이지
 @app.route('/login')
 def login():
     msg = request.args.get("msg")
@@ -51,7 +53,7 @@ def sign_in():
          'id': username_receive,
          'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
         }
-        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256').decode('utf-8')
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
 
         return jsonify({'result': 'success', 'token': token})
     # 찾지 못하면
@@ -61,23 +63,11 @@ def sign_in():
 
 
 
-@app.route('/sign_up/save', methods=['POST'])
-def sign_up():
-    # 회원가입
-    username_receive = request.form['username_give']
-    password_receive = request.form['password_give']
-    password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
-    # DB에 저장
-    return jsonify({'resultg': 'success'})
-
-
-
-@app.route('/')
-def main():
-    return render_template("main.html")
-
-
-
+# @app.route('/')
+# def main():
+#     return render_template("main.html")
+# -------------------------------
+#
 ## API 역할을 하는 부분
 @app.route('/main', methods=['POST'])
 def write_review():
@@ -97,20 +87,20 @@ def write_review():
 
 
 
-# #메인페이지
-# @app.route('/main')
-# def main():
-#     return render_template("main.html")
+#메인페이지
+@app.route('/main')
+def main():
+    return render_template("main.html")
 
 # 오늘의프로필 페이지
 @app.route('/profile')
 def profile():
     return render_template("profile.html")
 
-# 로그인페이지
-@app.route('/login')
-def login():
-    return render_template("login.html")
+# # 로그인페이지
+# @app.route('/login')
+# def login():
+#     return render_template("login.html")
 
 
 #오늘의프로필등록
